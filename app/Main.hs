14c05d918e08fe78e59d03991281ff79bc4bc62e
mejgun <mejgun@gmail.com>
2020-05-22 19:54:48 +0300

    test

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Network.Wai
+import           Network.HTTP.Types             ( status200 )
+import           Network.Wai.Handler.Warp       ( run )
+
+import           Blaze.ByteString.Builder       ( fromByteString )
+import           Blaze.ByteString.Builder.Char.Utf8
+                                                ( fromShow )
+import           Control.Concurrent.MVar
+--import           Data.Monoid                    ( (<>) )
+
+--import           Lib
+
+application
+  :: MVar Int
+  -> Request
+  -> (Response -> IO ResponseReceived)
+  -> IO ResponseReceived
+application countRef _ respond = do
+  modifyMVar countRef $ \count -> do
+    let count' = count + 1
+        msg    = fromByteString "You are visitor number: " <> fromShow count'
+    responseReceived <- respond
+      $ responseBuilder status200 [("Content-Type", "text/plain")] msg
+    return (count', responseReceived)
 
 main :: IO ()
-main = someFunc
+main = do
+  visitorCount <- newMVar 0
+  run 8080 $ application visitorCount
