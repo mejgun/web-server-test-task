c89887c5c11944aae162f3afa538730fbd206eec
mejgun <mejgun@gmail.com>
2020-05-27 22:49:36 +0300

    route func add

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ module Main
   )
 where
 
+import qualified Data.Text                     as T
+                                                ( Text )
 import           Network.Wai
 import           Network.Wai.Handler.Warp       ( run )
 import           Network.Wai.Middleware.RequestLogger
@@ -15,23 +17,24 @@ import           PG
 import           Types
 import           Users
 
-application :: MyApp
-application c r rd = case pathInfo r of
-  ["getusers"    ] -> getUsers c r rd
-  ["createuser"  ] -> createUser c r rd
-  ["deleteuser"  ] -> deleteUser c r rd
-  ["loginuser"   ] -> loginUser c r rd
-  ["makeauthor"  ] -> makeAuthor c r rd
-  ["editauthor"  ] -> editAuthor c r rd
-  ["deleteauthor"] -> deleteAuthor c r rd
-  ["getauthors"  ] -> getAuthors c r rd
-  _                -> rd responseERR
-
-
 main :: IO ()
 main = do
   conn <- pgconnect
   putStrLn "Server started"
   run 8080 $ logStdout $ application conn
 
+application :: MyApp
+application c r rd = case pathInfo r of
+  [x] -> (route x) c r rd
+  _   -> rd responseERR
 
+route :: T.Text -> MyApp
+route "getusers"     = getUsers
+route "createuser"   = createUser
+route "deleteuser"   = deleteUser
+route "loginuser"    = loginUser
+route "makeauthor"   = makeAuthor
+route "editauthor"   = editAuthor
+route "deleteauthor" = deleteAuthor
+route "getauthors"   = getAuthors
+route _              = \_ _ rd -> rd responseERR
