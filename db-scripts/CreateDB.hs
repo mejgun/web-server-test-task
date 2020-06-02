4799e17d4620e37d0fcfcad7bb5ee9f0d9fd108c
mejgun <mejgun@gmail.com>
2020-06-02 18:25:39 +0300

    fix news tags table unique(tag+news id)

--- db-scripts/CreateDB.hs
+++ db-scripts/CreateDB.hs
@@ -19,6 +19,6 @@ main = do
     , "create table if not exists tags (id serial primary key, name text not null unique);"
     , "create table if not exists news (id serial primary key, name varchar(999) not null, date date not null, author_id integer not null references authors(id) on delete restrict, category_id integer not null references categories(id) on delete restrict, text text not null, main_photo varchar(999), published bool not null default false);"
     , "create table if not exists news_photos (id serial primary key, news_id integer not null references news(id) on delete restrict, photo varchar(999) unique not null);"
-    , "create table if not exists news_tags (id serial primary key, tag_id integer not null references tags(id) on delete cascade, news_id integer references news(id) on delete cascade);"
+    , "create table if not exists news_tags (id serial primary key, tag_id integer not null references tags(id) on delete cascade, news_id integer not null references news(id) on delete cascade, constraint unique_news_tag unique (tag_id,news_id));"
     , "do $$ begin if not exists (select constraint_name as a from information_schema.constraint_column_usage where constraint_name = 'users_photo_key') then alter table if exists users add constraint users_photo_key unique (photo); end if; end; $$;"
     ]
