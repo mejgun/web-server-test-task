5f5ca0b96f317aaeb34b4313f0a65ff48d85b6b9
mejgun <mejgun@gmail.com>
2020-06-11 20:56:21 +0300

    get drafts continue

--- src/News/GetDrafts.hs
+++ src/News/GetDrafts.hs
@@ -7,6 +7,7 @@ module News.GetDrafts
 where
 
 
+import           Control.Monad                  ( liftM2 )
 import qualified Data.Aeson                    as A
 import           Data.Maybe                     ( catMaybes )
 import           Database.PostgreSQL.Simple.FromRow
@@ -20,39 +21,36 @@ import           PG
 import           Types
 
 data Draft = Draft
-    { draft_id         :: Int
-    , draft_date       :: String
-    , draft_name       :: String
-    , draft_text       :: String
-    , draft_main_photo :: Maybe String
-    , draft_photos     :: [Photo]
-    , draft_tags       :: [Tag]
+    { draft_id            :: Int
+    , draft_date          :: String
+    , draft_name          :: String
+    , draft_text          :: String
+    , draft_main_photo    :: Maybe String
+    , draft_photos        :: [Photo]
+    , draft_tags          :: [Tag]
+    , draft_category_id   :: Int
+    , draft_category_name :: String
     }
     deriving (Generic, Show)
 
 instance A.ToJSON Draft
 instance FromRow Draft where
-  fromRow = do
-    d1 <- field
-    d2 <- field
-    d3 <- field
-    d4 <- field
-    d5 <- field
-    d6 <- field
-    d7 <- field
-    d8 <- field
-    d9 <- field
-    return Draft
-      { draft_id         = d1
-      , draft_date       = d2
-      , draft_name       = d3
-      , draft_text       = d4
-      , draft_main_photo = d5
-      , draft_photos = map (\(x, y) -> Photo { photo_id = x, photo_url = y })
-                         $ zipPGarrays d6 d7
-      , draft_tags       = map (\(x, y) -> Tag { tag_id = x, tag_name = y })
-                             $ zipPGarrays d8 d9
-      }
+  fromRow =
+    Draft
+      <$> field
+      <*> field
+      <*> field
+      <*> field
+      <*> field
+      <*> liftM2 makePhoto field field
+      <*> liftM2 makeTag   field field
+      <*> field
+      <*> field
+   where
+    makePhoto :: PGArray (Maybe Int) -> PGArray (Maybe String) -> [Photo]
+    makePhoto a b = map (\(x, y) -> Photo x y) $ zipPGarrays a b
+    makeTag :: PGArray (Maybe Int) -> PGArray (Maybe String) -> [Tag]
+    makeTag a b = map (\(x, y) -> Tag x y) $ zipPGarrays a b
 
 data Photo = Photo
     { photo_id  :: Int
@@ -83,7 +81,7 @@ getDrafts conn _ = handleSqlErr $ do
   drafts <-
     query_
       conn
-      "select n.id,n.date::text,n.name,n.text,n.main_photo,array_agg(np.id) as photo_id,array_agg(np.photo) as photo,array_agg(nt.tag_id) as tag_id,array_agg(t.name) as tag_name from news as n left join news_photos as np on n.id=np.news_id left join news_tags as nt on nt.news_id=n.id left join tags as t on t.id=nt.tag_id group by n.id;" :: IO
+      "select n.id,n.date::text,n.name,n.text,n.main_photo,array_agg(np.id),array_agg(np.photo),array_agg(nt.tag_id),array_agg(t.name),n.category_id,c.name from news as n left join news_photos as np on n.id=np.news_id left join news_tags as nt on nt.news_id=n.id left join tags as t on t.id=nt.tag_id left join categories as c on c.id=n.category_id group by n.id, c.name;" :: IO
       [Draft]
   return $ respJSON drafts
 
